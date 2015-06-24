cd078297ff07fbf35d0773c6120148f138b94c0c
Diego Rodriguez <diero17@gmail.com>
2015-06-24 02:05:37 -0300

    ajax

--- js/Ajax.js
+++ js/Ajax.js
@@ -32,6 +32,57 @@ $(document).ready(function(){
     })
 });
 
+$(document).ready(function(){
+    $("#eventos").on("click",function(event){
+        event.preventDefault();
+        $.ajax({
+            type: "GET",
+            dataType: "html",
+            url: "Eventos.html",
+            success: function(data){
+                $("#contenido").html(data);
+            },
+            error: function(){
+                alert("error");
+            }
+        })
+    })
+});
+
+$(document).ready(function(){
+    $("#productos").on("click",function(event){
+        event.preventDefault();
+        $.ajax({
+            type: "GET",
+            dataType: "html",
+            url: "Productos.html",
+            success: function(data){
+                $("#contenido").html(data);
+            },
+            error: function(){
+                alert("error");
+            }
+        })
+    })
+});
+
+$(document).ready(function(){
+    $("#registro").on("click",function(event){
+        event.preventDefault();
+        $.ajax({
+            type: "GET",
+            dataType: "html",
+            url: "Registro.html",
+            success: function(data){
+                $("#contenido").html(data);
+            },
+            error: function(){
+                alert("error");
+            }
+        })
+    })
+});
+
 function enviarEinsertar(){
     var descripcion = $("#descripcion").val();
     var precio = $("#precio").val();
@@ -79,4 +130,29 @@ function getInformationByGroup(){
 
         }
     });
+}
+//------------------------JUEGO------------------------------//
+
+"use strict";
+
+var caras =[3,4,5,6,7,8];
+
+function tirarDados() {
+    var num = Math.floor(Math.random()*(caras[Math.floor(Math.random()*6)] ))+1;
+    return num;
+}
+
+function tirarVarios(){
+    var tiradas = [];
+    var sumayor = 0;
+    for (var i = 0; i < document.getElementById('cantDados').value; i++) {
+        var suma = tirarDados();
+        tiradas.push(suma);
+        tiradas[suma-2]++;
+    }
+    for (var x = 0; x <tiradas.length; x++){
+        sumayor += tiradas[x];
+    }
+    $("#SumaMayor").val(sumayor);
+    $('#ValoresTiradas').val(tiradas);
 }
